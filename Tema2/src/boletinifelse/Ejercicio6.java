b58fb37b66d08e09102bbf1e81db2621a65681c1
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-10-08 20:06:27 +0200

    Ej 6 corregido, tras corregirlo es clase

--- Tema2/src/boletinifelse/Ejercicio6.java
+++ Tema2/src/boletinifelse/Ejercicio6.java
@@ -30,6 +30,10 @@ public class Ejercicio6 {
 		// ecuación de segundo grado.
 		double x2;
 
+		// Creamos la variable discriminante como double para almacenar la solución de
+		// la raiz cuadrada.
+		double discriminante;
+
 		// Le pedimos al usuario que introduzca el valor de "a" y lo leemos.
 		System.out.print("Introduce el valor de a --> ");
 		a = sc.nextInt();
@@ -42,32 +46,30 @@ public class Ejercicio6 {
 		System.out.print("Introduce el valor de c --> ");
 		c = sc.nextInt();
 
-		// Resolvemos la primera solución de la ecuación de segundo grado con la
-		// ecuación, menos b mas la raiz cuadrada de b al cuadrado - 4 por a por c y
-		// todo eso entre 2 por a.
-		x1 = ((-1 * b) + (Math.sqrt(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
-
-		// Resolvemos la segunda solución de la ecuación de segundo grado con la
-		// ecuación, menos b menos la raiz cuadrada de b al cuadrado - 4 por a por c y
-		// todo eso entre 2 por a.
-		x2 = ((-1 * b) - (Math.sqrt(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
-
-		// Comprobamos si las soluciones son las mismas porque si es así esta ecuación
-		// solo tiene una solución.
-		if (x1 == x2) {
-			System.out.println();
+		// Comprobamos si a es cero, si es así nuestra ecuación solo tendra una
+		// solución, ya que sera una ecuación de primer grado. Solo habría que resolver
+		// una x.
+		if (a == 0) {
+			x1 = (double) -c / b;
 			System.out.println("Esta ecuación tiene una sola solución = " + x1);
-			// Con la condición else-if comprobamos si los resultados de la ecuación tienen
-			// números reales y si es así los mostramos.
-		} else if ((x1 <= 0 || x1 > 0) || (x2 <= 0 || x2 > 0)) {
-			System.out.println();
-			System.out.println("Estas son las dos soluciones = " + x1 + ", " + x2);
-			// Si no se cumple ninguna de las condiciones anteriores es que nuestro ecuación
-			// no tiene resultados reales, por ello mostramos el mensaje de que nuestra
-			// ecuación no tiene resultado.
+
+			// Si la a es distinto de 0 estonces hacemos lo siguiente:
 		} else {
-			System.out.println();
-			System.out.println("La ecuación no tiene soluciones reales.");
+			// Calculamos el discriminante, lo que hay dentro de la raiz cuadrada.
+			discriminante = b * b - 4 * a * c;
+
+			// Si es menor que 0 la ecuación no tiene solución, porque no se puede hacer la
+			// raiz cuadra de un número negativo.
+			if (discriminante < 0) {
+				System.out.println();
+				System.out.println("La ecuación no tiene soluciones reales.");
+			//Pero si ese discriminante es positivo, entonces calculamos x1 (primera solución) y x2 (segunda solución).
+			} else {
+				x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+				x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+				System.out.println();
+				System.out.println("Estas son las dos soluciones = " + x1 + ", " + x2);
+			}
 		}
 
 		// Cierre de Scanner
